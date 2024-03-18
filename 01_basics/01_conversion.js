372e88e1fa0daedc2cddc6df199319842c9d1d62
supriyacode17 <100865347+supriyacode17@users.noreply.github.com>
2024-03-18 16:52:34 +0000

    conversion

---
+++ 01_basics/01_conversion.js
@@ -0,0 +1,26 @@
+let score = null;
+
+console.log(typeof(score));
+
+let valueInNumber= Number(score);
+console.log( typeof valueInNumber);
+console.log( valueInNumber);
+
+// number => "33" = 33
+// "455ty" => NaN
+//"Supriya" => NaN
+// undefined => NaN
+//null => 0
+//true => 1
+
+let isLoggedIn = NaN
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn)
+
+let someNumber = undefined
+
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber)
+
+//*************Operation********************* */
